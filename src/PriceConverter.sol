741196182122da3d056cc8907d5dec33dde490d4
kxzb-fun <sunday_ever@163.com>
2024-07-01 18:48:46 +0800

    fixed: change AggregatorV3Interface import url

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts@1.1.1/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
     function getPrice() internal view returns (uint256) {
@@ -11,8 +11,13 @@ library PriceConverter {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(
             0x694AA1769357215DE4FAC081bf1f309aDC325306
         );
-        (, /* uint80 roundId */ int256 answer, , , ) = /* uint256 startedAt */ /* uint256 updatedAt */ /* uint80 answeredInRound */
-        priceFeed.latestRoundData();
+        (
+            ,
+            /* uint80 roundId */ int256 answer /* uint256 startedAt */ /* uint256 updatedAt */ /* uint80 answeredInRound */,
+            ,
+            ,
+
+        ) = priceFeed.latestRoundData();
         return uint256(answer * 1e10);
     }
 
