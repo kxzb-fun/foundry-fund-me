8dba4d19f24b067e40fc76f6cf46839106edec9e
kxzb-fun <sunday_ever@163.com>
2024-07-04 15:59:26 +0800

    feat: mock different data

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -2,13 +2,16 @@
 pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     FundMe fundMe;
 
     function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetowrkConfig();
         vm.startBroadcast();
-        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig {
+    // If we are on a local Anvil, we deploy the mocks
+    // Else, grab the existing address from the live network
+    NetworkConfig public activeNetowrkConfig;
+
+    constructor() {
+        // https://chainlist.org/?testnets=true&search=sepolia
+        if (block.chainid == 11155111) {
+            activeNetowrkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetowrkConfig = getMainnetEthConfig();
+        } else {
+            activeNetowrkConfig = getAnvilEthConfig();
+        }
+    }
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed address
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory mainnetConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return mainnetConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
+}
